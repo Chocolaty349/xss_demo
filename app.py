7e34b165d75c9e8eea2140230f1a14a02139fdae
chocolaty <vietanhkhong@outlook.com>
2024-07-11 14:48:29 +0700

    add cookie to app, add keylog demo, add newapp to remove later

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import db
 
 app = Flask(__name__)
+app.secret_key = 'blablakey'
+
+app.config.update(
+    SESSION_COOKIE_HTTPONLY=False,
+    SESSION_COOKIE_SAMESITE='Lax'  
+)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if 'user_id' not in session:
+        session['user_id'] = gen_userId()
+        
     if request.method == 'POST':
         db.add_comment(request.form['comment'])
-
+        return redirect(url_for('index'))
+        
     search_query = request.args.get('q')
-
     comments = db.get_comments(search_query)
 
     return render_template('index.html',
                            comments=comments,
-                           search_query=search_query)
+                           search_query=search_query,
+                           )
 
+def gen_userId():
+    import uuid
+    return str(uuid.uuid4())
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
